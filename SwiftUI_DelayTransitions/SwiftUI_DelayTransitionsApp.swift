81b1925d824946b8342b1fff39d82afd13fae37b
paku <boardguy1024@gmail.com>
2023-12-03 20:52:37 +0900

    Initial Commit

---
+++ SwiftUI_DelayTransitions/SwiftUI_DelayTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DelayTransitionsApp.swift
+//  SwiftUI_DelayTransitions
+//
+//  Created by paku on 2023/12/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DelayTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
